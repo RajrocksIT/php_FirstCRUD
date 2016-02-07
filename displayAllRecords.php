02c667aa086f5592c06c73cc2b684c668251dc77
Praviin M <pravsm@gmail.com>
2016-02-07 14:19:58 -0500

    Added displayAllRecords.php page. - page layout only - page doesnt do anything.

---
+++ displayAllRecords.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Praviin
+ * Date: 2/7/16
+ * Time: 2:17 PM
+ */
+
+?>
+
+<html>
+<head>
+  <title>
+    Display Records
+  </title>
+</head>
+<body>
+  <h1>This page will display all the records from our database table with Update / Delete Options</h1>
+</body>
+</html>
