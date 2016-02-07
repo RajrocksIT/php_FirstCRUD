e0090ff3754238c6b0b2249f0d84bbf37edd0977
Praviin M <pravsm@gmail.com>
2016-02-07 14:46:59 -0500

    Added Config.php and added database settings - connecting to UserManager table.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+
+require_once("db-settings.php"); //Require DB connection
+
+
+?>
--- index.php
+++ index.php
@@ -6,9 +6,10 @@
  * Time: 1:55 PM
  */
 ?>
-
-<html>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
   <title>
     FIRST CRUD
   </title>
@@ -53,25 +54,27 @@
 </head>
 <body>
 
-<!-- Table goes in the document BODY -->
-<table class="table-style-three">
-	<thead>
-	<!-- Display CRUD options in TH format -->
-    <tr>
-      <th><a href="createNewRecord.php">Create a new record (C)</a></th>
-    </tr>
-    <tr>
-      <th><a href="displayAllRecords.php">Read All Record information (R)</a></th>
-    </tr>
-    <tr>
-      <th><a href="displayAllRecords.php">Update A Record (U)</a></th>
-    </tr>
-    <tr>
-      <th><a href="displayAllRecords.php">Delete A Record (D)</a></th>
-    </tr>
+  <?php require_once("config.php"); ?>
 
-	</thead>
-</table>
+  <!-- Table goes in the document BODY -->
+  <table class="table-style-three">
+      <thead>
+      <!-- Display CRUD options in TH format -->
+      <tr>
+        <th><a href="createNewRecord.php">Create a new record (C)</a></th>
+      </tr>
+      <tr>
+        <th><a href="displayAllRecords.php">Read All Record information (R)</a></th>
+      </tr>
+      <tr>
+        <th><a href="displayAllRecords.php">Update A Record (U)</a></th>
+      </tr>
+      <tr>
+        <th><a href="displayAllRecords.php">Delete A Record (D)</a></th>
+      </tr>
+
+      </thead>
+  </table>
 
 </body>
 </html>
